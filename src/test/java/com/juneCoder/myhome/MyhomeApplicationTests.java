111011f8abf29787707b53fcd367f4e9d4c5e230
jmyu0423 <dbwnsaud@gmail.com>
2023-12-11 14:28:14 +0900

    init

---
+++ src/test/java/com/juneCoder/myhome/MyhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juneCoder.myhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
